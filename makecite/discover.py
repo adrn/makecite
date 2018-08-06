6d219b0d4670caef768e5c1b7bef499b17b1351a
apmechev <apmechev@gmail.com>
2018-08-06 15:45:57 +0200

    docstrings

--- makecite/discover.py
+++ makecite/discover.py
@@ -7,6 +7,12 @@ from os import walk
 def find_all_files(root_path=None):
     """Find all relevant files in the current directory 
     and returns a dictionary, grouping files by type (.ipynb, .py)
+
+    :param root_path: The path where to search for the python files 
+                    (Default:None=> From current pwd)
+    :type root_path: str
+    :return: dictionary with keys = file extensions
+    :rtype: dict
     """
     if not root_path: 
         root_path = os.getcwd()
@@ -18,3 +24,7 @@ def find_all_files(root_path=None):
                 if _file.endswith("."+ext):
                     _files[ext].append(_file)
     return _files
+
+
+
+
