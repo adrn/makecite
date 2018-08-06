a6dc955e2e3cbbae0032c02faab6ce182b7972b8
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-08-06 16:27:20 +0200

    pep8 and switch to numpydoc

--- makecite/discover.py
+++ makecite/discover.py
@@ -1,31 +1,33 @@
-"""makecite.discover: Discover all imports used in all python files in the current folder. 
+"""makecite.discover: Discover all imports used in all python files in the current folder.
 """
 import os
 from os.path import expanduser
 from os import walk
 
 def find_all_files(root_path=None):
-    """Find all relevant files in the current directory 
+    """Find all relevant files in the current directory
     and returns a dictionary, grouping files by type (.ipynb, .py)
 
-    :param root_path: The path where to search for the python files 
-                    (Default:None=> From current pwd)
-    :type root_path: str
-    :return: dictionary with keys = file extensions
-    :rtype: dict
+    Parameters
+    ----------
+    root_path : str, optional
+        The path where to search for the python files. (Default:None => From
+        current pwd).
+
+    Returns
+    -------
+    files : dict
+        A dictionary with keys = file extensions
     """
-    if not root_path: 
+    if not root_path:
         root_path = os.path.abspath(os.getcwd())
-    print(root_path)
+
     root_path = expanduser(root_path)
-    _files={'py':[], 'ipynb':[]}
+    _files = {'py': [], 'ipynb': []}
     for root, dirs, files in os.walk(root_path):
         for _file in files:
             for ext in list(_files):
                 if _file.endswith("."+ext):
                     _files[ext].append(_file)
+                    
     return _files
-
-
-
-
