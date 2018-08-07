3dd1498085c61c3da9049716a8382c079102bc79
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-08-07 09:32:37 +0200

    Tidying up setup

--- setup.py
+++ setup.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python
-"""Setup script for the makecite package
-"""
-#from distutils.core import setup
+
+"""Setup script for the makecite package """
+
 from setuptools import setup
-import makecite
+
+from makecite import __version__ as version
 
 setup(name='makecite',
       packages=[],
-      version=makecite.__version__,
+      version=version,
       setup_requires=[],
       tests_require=[],
       include_package_data=True,
-      description='Package for making bibtex citation strings for common python packages',
-      long_description="Searching through a folder's python and python Notebook "
-      "files and creates a bibtex string for all used packages",
-      author='Adrian Price-Whelan, Alexandar Mechev, Julia Melo Rodrigues de Aguiar',
-      author_email='LOFAR@apmechev.com',
+      description='Autogenerate bibtex citations for common Python packages',
+      long_description=("Searches through a Python and Jupyter Notebook files "
+                        "and creates a bibtex citation string for all " "packages used."),
+      author=('Adrian Price-Whelan, '
+              'Alexandar Mechev, '
+              'Julia Melo Rodrigues de Aguiar'),
+      author_email='adrn@astro.princeton.edu',
       url='https://www.github.com/adrn/makecite',
       download_url='https://github.com/adrn/makecite/archive/v0.0.1.tar.gz',
-      keywords=['LaTeX','Astronomy','Citation','package management'],
+      keywords=['LaTeX', 'Astronomy', 'Citation', 'package management'],
       classifiers=['Development Status :: 1 - Planning',
                    'Intended Audience :: Developers',
                    'Intended Audience :: Science/Research',
                    "License :: OSI Approved :: MIT License",
                    "Natural Language :: English",
                    "Topic :: Scientific/Engineering :: Astronomy",
-                   'Programming Language :: Python :: 2.6',
-                   'Programming Language :: Python :: 2.7',
                    'Programming Language :: Python :: 3',
                    'Programming Language :: Python :: 3.2',
                    'Programming Language :: Python :: 3.3',
@@ -34,5 +35,4 @@ setup(name='makecite',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6'
                    ]
-     )
-
+    )
