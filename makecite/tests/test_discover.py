5197cdc3c44d5fc292c09722e647c1decd1192c4
apmechev <apmechev@gmail.com>
2018-08-07 14:36:40 +0200

    cleaning test_discove for pylint

--- makecite/tests/test_discover.py
+++ makecite/tests/test_discover.py
@@ -1,14 +1,13 @@
-import os
+from makecite import discover
 import makecite
 
-import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../../')
-from makecite import discover
+import os
+import sys
 
 def test_discover():
+    """Test finding the files in the module's directory"""
     bpath = os.path.dirname(os.path.abspath(makecite.__file__))
-    discovered = discover.find_all_files(bpath) 
+    discovered = discover.find_all_files(bpath)
     assert 'discover.py' in [x.split('/')[-1] for x in discovered['.py']]
     assert  '__init__.py' in [x.split('/')[-1] for x in discovered['.py']]
     assert 'test_discover.py'in [x.split('/')[-1] for x in discovered['.py']]
