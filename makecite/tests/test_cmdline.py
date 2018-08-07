c914cf1a507c06f5300f2d34a5cfc5f718e74b05
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-08-07 21:09:06 +0200

    improve coverage

--- makecite/tests/test_cmdline.py
+++ makecite/tests/test_cmdline.py
@@ -5,34 +5,45 @@ from os import path
 import pytest
 
 # Package
-from ..cmdline import get_all_packages
+from ..cmdline import get_all_packages, get_bibtex, main
 
-_path = path.split(path.abspath(__file__))[0]
+_path = path.join(path.split(path.abspath(__file__))[0], 'data')
 
 
 def test_readme_examples():
     # makecite my_script.py
-    pkgs = get_all_packages(path.join(_path, 'data/file1.py'))
+    pkgs = get_all_packages(path.join(_path, 'file1.py'))
     expected_pkgs = ['os', 'sys']
     assert all([x in pkgs for x in expected_pkgs])
 
     # makecite --ext=.py .
-    pkgs = get_all_packages(path.join(_path, 'data/'),
-                            extensions=['.py'])
+    pkgs = get_all_packages(_path, extensions=['.py'])
     expected_pkgs = ['os', 'sys', 'pickle', 're', 'datetime']
     assert all([x in pkgs for x in expected_pkgs])
 
     # makecite --ext=.py --ext=.ipynb my_code my_notebooks
     pkgs = get_all_packages(
-        [path.join(_path, 'data/other_scripts/moar_stuff'),
-         path.join(_path, 'data/other_scripts/even_moar_stuff')],
+        [path.join(_path, 'other_scripts/moar_stuff'),
+         path.join(_path, 'other_scripts/even_moar_stuff')],
         extensions=['.py', '.ipynb'])
     expected_pkgs = ['os', 'sys', 're', 'pickle', 'datetime']
     assert all([x in pkgs for x in expected_pkgs])
 
 
+def test_bibtex():
+
+    bibtex = get_bibtex('astropy')
+    assert isinstance(bibtex, str)
+
+    with pytest.raises(ValueError):
+        get_bibtex('some_fake_package')
+
+
+def test_cli():
+    main({'paths': _path})
+
+
 def test_failures():
 
     with pytest.raises(ValueError):
-        get_all_packages(path.join(_path, 'data/'),
-                         extensions=['.py', '.rst'])
+        get_all_packages(_path, extensions=['.py', '.rst'])
