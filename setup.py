c12aa95efef42c8882413fd237ede7b56ef24d03
Adrian Price-Whelan <adrian.prw@gmail.com>
2018-08-07 10:26:27 +0200

    add entrypoint for makecite script

--- setup.py
+++ setup.py
@@ -6,6 +6,13 @@ from setuptools import setup
 
 from makecite import __version__ as version
 
+# Add entry point for `makecite` to be run as a script:
+entry_points = {}
+entry_points['console_scripts'] = [
+    'makecite = makecite.cmdline:main',
+]
+
+
 setup(name='makecite',
       packages=[],
       version=version,
@@ -34,5 +41,6 @@ setup(name='makecite',
                    'Programming Language :: Python :: 3.4',
                    'Programming Language :: Python :: 3.5',
                    'Programming Language :: Python :: 3.6'
-                   ]
+                   ],
+      entry_points=entry_points
     )
